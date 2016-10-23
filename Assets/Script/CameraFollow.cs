e330e538e4bb375c3795c033e9cae217d708e3de
Hegen44 <hegen.lo@gmail.com>
2016-10-23 03:07:30 -0700

    first commit

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float scale;
+    public float smoothness;
+    Camera mycam;
+
+	// Use this for initialization
+	void Start () {
+
+        mycam = GetComponent<Camera>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    mycam.orthographicSize = (Screen.height / 100f) / scale;
+
+        if (target)
+        {
+            //transform.position = Vector3.Lerp(From, To, how fast);
+            transform.position = Vector3.Lerp(transform.position, target.position, smoothness) + new Vector3(0,0,-10);
+        }
+	}
+}
