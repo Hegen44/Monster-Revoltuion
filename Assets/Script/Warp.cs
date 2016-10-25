6e644bbf2d8188919269ae164e42b4836a0f2d26
Hiu-Chun Lo <hilo@crm.ucsc.edu>
2016-10-24 19:07:46 -0700

    still have bug on fading

---
+++ Assets/Script/Warp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warp : MonoBehaviour {
+
+	public Transform warpTarget;
+
+	IEnumerable OnTriggerEnter2D(Collider2D other){
+
+        Debug.Log("fading");
+
+        ScreenFader sf = GameObject.FindGameObjectWithTag("Fader").GetComponent<ScreenFader>();
+
+        yield return StartCoroutine(sf.FadeToBlack());
+
+        Debug.Log("wraping");
+
+        other.gameObject.transform.position = warpTarget.position;
+        Camera.main.transform.position = warpTarget.position;
+
+        yield return StartCoroutine(sf.FadeToClear());
+
+    }
+}
