7aec517a749deffbcc1d2e632434c36d0aaf3764
Hiu-Chun Lo <hilo@crm.ucsc.edu>
2016-11-08 16:59:03 -0800

    invnetory cont

---
+++ Assets/Script/Inventory.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+    GameObject inventoryPanel;
+    GameObject slotPanel;
+    ItemDatabase database;
+    public GameObject inventorySlot;
+    public GameObject inventoryItem;
+
+    public int slotAmount = 20;
+    public List<Item> items = new List<Item>();
+    public List<GameObject> slots = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        database = GetComponent<ItemDatabase>();
+        inventoryPanel = GameObject.Find("Inventory Panel");
+        slotPanel = inventoryPanel.transform.FindChild("Slot Panel").gameObject;
+        for (int i = 0; i < slotAmount; ++i)
+        {
+            items.Add(new Item());
+            slots.Add(Instantiate(inventorySlot));
+            slots[i].transform.SetParent(slotPanel.transform);
+        }
+
+        AddItem(0);
+        AddItem(1);
+	}
+
+    public void AddItem(int id)
+    {
+        Item itemToAdd = database.FetchItemByID(id);
+
+        for (int i = 0; i < items.Count; ++i)
+        {
+            if (items[i].ID == -1)
+            {
+                items[i] = itemToAdd;
+                GameObject itemObj = Instantiate(inventoryItem);
+                itemObj.transform.SetParent(slots[i].transform);
+                itemObj.GetComponent<Image>().sprite = itemToAdd.image;
+                itemObj.transform.position = Vector2.zero;
+                itemObj.name = itemToAdd.Title;
+                break;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Script/ItemDatabase.cs
+++ Assets/Script/ItemDatabase.cs
@@ -1,15 +1,81 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using LitJson;
 
 public class ItemDatabase : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private List<Item> database = new List<Item>();
+    private JsonData itemData;
+
+    void Start()
+    {                                                  //unity location.path to file
+        itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAsserts/Items.json"));
+        ConstructItemDatabase();
+    }
+
+    public Item FetchItemByID(int id)
+    {
+        for (int i = 0; i< database.Count; ++i)
+        {
+            if(database[i].ID == id)
+                return database[i];
+        }
+        return null;
+    }
+    
+    void ConstructItemDatabase()
+    {
+        for ( int i = 0; i< itemData.Count; ++i)
+        {
+            database.Add(new Item((int)itemData[i]["id"], (string)itemData[i]["title"], (int)itemData[i]["value"], 
+                (string)itemData[i]["description"], (string)itemData[i]["slug"], (bool)itemData[i]["stackable"]));
+        }
+    }
+
+}
+
+public class Item
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public int Value { get; set; }
+    public string Description { get; set; }
+    public string Slug { get; set; }
+    public bool Stackable { get; set; }
+    public int Power { get; set; }
+    public int Defence { get; set; }
+    public int Vitality { get; set; }
+    public Sprite image { get; set; }
+
+    public Item(int id, string title, int value, string description, string slug, bool stackable)
+    {
+        this.ID = id;
+        this.Title = title;
+        this.Value = value;
+        this.Description = description;
+        this.Slug = slug;
+        this.Stackable = stackable;
+        this.image = Resources.Load<Sprite>("Sprites/Inventory/Item/" + slug);
+    }
+
+    public Item(int id, string title, int value, string description, string slug, bool stackable, int power,int defence,int vitality)
+    {
+        this.ID = id;
+        this.Title = title;
+        this.Value = value;
+        this.Description = description;
+        this.Slug = slug;
+        this.Stackable = stackable;
+        this.Power = power;
+        this.Defence = defence;
+        this.Vitality = vitality;
+        this.image = Resources.Load<Sprite>("Sprites/Inventory/Item/" + slug);
+    }
+
+    public Item()
+    {
+        this.ID = -1; // empty item
+    }
 }
