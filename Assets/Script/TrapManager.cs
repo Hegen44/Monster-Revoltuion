d9aecaacdfade8d4cbdecea06122c7714a59328c
Hiu-Chun Lo <hilo@crm.ucsc.edu>
2016-11-10 20:12:43 -0800

    second update done

--- Assets/Script/TrapManager.cs
+++ Assets/Script/TrapManager.cs
@@ -5,17 +5,21 @@ public class TrapManager : MonoBehaviour {
 
     Inventory inv;
     ItemDatabase database;
+    SpriteRenderer sr;
     public GameObject trap;
 
     // Use this for initialization
     void Start () {
         inv = GameObject.Find("Inventory").GetComponent<Inventory>();
         database = GameObject.Find("Inventory").GetComponent<ItemDatabase>();
+        sr = GetComponent<SpriteRenderer>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(1))
+        sr.color = new Color(1, 1, 1, 0);
+        if (inv.items.Contains(database.FetchItemByID(1))) sr.color = new Color(1, 1, 1, 0.2f);
+            if (Input.GetMouseButtonDown(1))
         {
             if (inv.items.Contains(database.FetchItemByID(1)))
             {
