c9cb669d7c1d95e335e2e2472e0ca4653b16b47b
Hiu-Chun Lo <hilo@crm.ucsc.edu>
2016-11-22 14:56:00 -0800

    hotfix 4

---
+++ Assets/Script/GameOver.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+    Text texts;
+    public HealthMananger Player;
+    private GameObject Hero;
+
+	void Start()
+    {
+
+        texts = GetComponent<Text>();
+        texts.enabled = false;
+    }
+	// Update is called once per frame
+	void Update () {
+
+        Hero = GameObject.FindGameObjectWithTag("Hero");
+        if (Player.currentHealth <= 0){
+            texts.enabled = true;
+            texts.text = "GAME OVER";
+        }
+        else if (Hero != null && Hero.GetComponent<HealthMananger>().currentHealth <= 0)
+        {
+            texts.enabled = true;
+            texts.text = "VICTORY";
+        }
+    }
+}
